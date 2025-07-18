40c4502d6d381115f77781b0a642c4dc62e61b28
Newsstate <newsstate6294@gmail.com>
2025-07-18 18:25:52 +0530

    Delete utils directory

--- utils/image_utils.py
+++
@@ -1,8 +0,0 @@
-import os
-
-def save_image(image_file, upload_folder):
-    if not os.path.exists(upload_folder):
-        os.makedirs(upload_folder)
-    filepath = os.path.join(upload_folder, image_file.filename)
-    image_file.save(filepath)
-    return filepath
